dd216896e598fd4dd56b6cd3c9535eaf58b5ef4e
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2021-08-17 17:33:52 +0300

    changes file

--- cod_for_file.py
+++ cod_for_file.py
@@ -1,32 +1,31 @@
-file_list = []
-def count_file(input_file_1, input_file_2, input_file_3):
-
- with open('1.txt')as file_1:
-    counter_1 = 0
-    counter_1 += sum(1 for line in file_1)
-    file_list.append(counter_1)
-
- with open('2.txt')as file_2:
-    counter_2 = 0
-    counter_2 += sum(1 for line in file_2)
-    file_list.append(counter_2)
-
- with open('3.txt')as file_3:
-    counter_3 = 0
-    counter_3 += sum(1 for line in file_3)
-    file_list.append(counter_3)
-
-
-    print(sorted(file_list))
-
-count_file('1.txt', '2.txt', '3.txt')
-
-def write_file(input_file):
-    with open('4.txt','w')as file:
-        file.write("Файл 2.txt \n1 строка \n\nФайл 1.txt \n8 строк\n\nФайл 3.txt \n9 строк")
-
-
-write_file('4.txt')
-
-with open('4.txt') as file:
-    print(file.read())
+import os
+class FileMaker():
+    def make_merged_file(self):
+        list_of_files = os.listdir('files')
+        files_content = {}
+        lines_count = []
+        for file in list_of_files:
+            f = open('files/' + file, "r")
+            number_of_lines = 0
+            lines = []
+            for line in f:
+                number_of_lines += 1
+                lines.append(line)
+            files_content[file] = {number_of_lines:  lines}
+            lines_count.append(number_of_lines)
+            f.close()
+        result_file = open("result.txt", "w+")
+        lines_count.sort()
+        for number in lines_count:
+            for file_name, content in files_content.items():
+                for count, lines in content.items():
+                    if number == count:
+                        result_file.writelines(file_name)
+                        result_file.writelines('\n')
+                        result_file.writelines(str(count))
+                        result_file.writelines('\n')
+                        result_file.writelines(lines)
+                        result_file.writelines('\n')
+        result_file.close()
+my_file_maker = FileMaker()
+my_file_maker.make_merged_file()
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 class Cook_book():
+
     def my_cook_book(self, input_file):
-        cook_book ={}
         with open('riceptes.txt', 'r') as menu:
+            self.cook_book = {}
             for line in menu:
                 dish_name = line.strip()
-                ricept = int(menu.readline().strip())
+                reciptes = int(menu.readline().strip())
                 ingredients_dish = []
-                for count in range(ricept):
+                for count in range(reciptes):
                     string_data = menu.readline().strip()
                     string_data = ''.join(string_data.split())
                     string_data = string_data.split('|')
-                    ingredients = {'ingredient_name': string_data[0], 'quantity': int(string_data[1]), 'measure': string_data[2]}
+                    ingredients = {'ingredient_name': string_data[0], 'quantity': int(string_data[1]),
+                               'measure': string_data[2]}
                     ingredients_dish.append(ingredients)
-                    cook_book[dish_name] = ingredients_dish
-            menu.readline()
-            return print(cook_book)
+                    self.cook_book[dish_name] = ingredients_dish
+                menu.readline()
+            return print(self.cook_book)
+
+
     def get_shop_list_by_dishes(self, dishes, person_count):
-        # if getattr('cook_book', 'No') == 'No':
-        #     print('Импортируйте файл кулинарной книги, перед тем, как формировать лист покупок!')
-        # else:
+        if getattr(self, 'cook_book', 'No') == 'No':
+            print('Импортируйте файл кулинарной книги, перед тем, как формировать лист покупок!')
+        else:
             result = {}
             for dish in dishes:
-                if dish in cook_book:
-                    new_shop_list = cook_book[dish]
+                if dish in self.cook_book:
+                    new_shop_list = self.cook_book[dish]
                     for element in new_shop_list:
                         ingredient = element['ingredient_name']
                         measure = element['measure']
@@ -30,8 +34,9 @@ class Cook_book():
                         result[ingredient] = {measure: quantity}
                 else:
                     print('Такого рецепта в книге еще нет.')
-                    return print(result)
+        return print(result)
+
 
 book = Cook_book()
 book.my_cook_book('riceptes.txt')
-# book.get_shop_list_by_dishes(['Омлет', 'Утка по-пекински'], 5)
+book.get_shop_list_by_dishes(['Омлет', 'Утка по-пекински'], 10)
